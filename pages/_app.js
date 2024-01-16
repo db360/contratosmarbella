6db668a598714550de64552f241848005f2c2fb2
David Martinez <da.b@hotmail.es>
2024-01-16 19:11:20 +0100

    cambio analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,31 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import { DataProvider } from "../context/dataContext";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <DataProvider>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ID_ANALYTICS}`}
+      />
+      <Script
+        id="analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', ${process.env.NEXT_PUBLIC_ID_ANALYTICS}, {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
